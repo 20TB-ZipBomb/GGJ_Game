39f480afd6d0e27913b82dc82c75a0b573d6f4df
Mohammed Bajaman <mohammed_bajaman@hotmail.com>
2024-01-26 20:44:41 -0800

    Connect on Start Game button click

--- Source/GGJ_Game/Private/Core/GGJ_GameInstance.cpp
+++ Source/GGJ_Game/Private/Core/GGJ_GameInstance.cpp
@@ -8,11 +8,14 @@ void UGGJ_GameInstance::Init()
 {
 	Super::Init();
 
-	if (!FModuleManager::Get().IsModuleLoaded("WebSockets")) 
+	if (!FModuleManager::Get().IsModuleLoaded("WebSockets"))
 	{
 		FModuleManager::Get().LoadModule("WebSockets");
-	
 	}
+}
+
+void UGGJ_GameInstance::OnConnectButtonPressed()
+{
 
 	WebSocket = FWebSocketsModule::Get().CreateWebSocket("ws://localhost:8080");
 
@@ -22,40 +25,40 @@ void UGGJ_GameInstance::Init()
 
 	//Runs whenever the websocket is connected to. Attach to the 'OnConnected' event
 	WebSocket->OnConnected().AddLambda([]()
-	{
-		GEngine->AddOnScreenDebugMessage(-1, -15.0f, FColor::Green, "Successfully connected");
-		UE_LOG(LogTemp, Warning, TEXT("Successfully connected"));
+		{
+			GEngine->AddOnScreenDebugMessage(-1, -15.0f, FColor::Green, "Successfully connected");
+			UE_LOG(LogTemp, Warning, TEXT("Successfully connected"));
 
-	});
+		});
 
 	//Event that triggers if an error occurs whilst connected
 	WebSocket->OnConnectionError().AddLambda([](const FString& Error)
-	{
-		GEngine->AddOnScreenDebugMessage(-1, -15.0f, FColor::Red, Error);
-		UE_LOG(LogTemp, Warning, TEXT("%s"), *Error);
-	});
+		{
+			GEngine->AddOnScreenDebugMessage(-1, -15.0f, FColor::Red, Error);
+			UE_LOG(LogTemp, Warning, TEXT("%s"), *Error);
+		});
 
 	//Event that triggers upon closing the connection.
 	WebSocket->OnClosed().AddLambda([](int32 StatusCode, const FString& Reason, bool bWasClean)
-	{
-		GEngine->AddOnScreenDebugMessage(-1, -15.0f, bWasClean ? FColor::Green : FColor::Red, "Connection closed" + Reason);
-		UE_LOG(LogTemp, Warning, TEXT("Connection closed: %s"), *Reason);
-	});
+		{
+			GEngine->AddOnScreenDebugMessage(-1, -15.0f, bWasClean ? FColor::Green : FColor::Red, "Connection closed" + Reason);
+			UE_LOG(LogTemp, Warning, TEXT("Connection closed: %s"), *Reason);
+		});
 
 	//Event that triggers upon recieving a string message
 	WebSocket->OnMessage().AddLambda([](const FString& Message)
-	{
-		GEngine->AddOnScreenDebugMessage(-1, -15.0f, FColor::Cyan, "Recieved Message" + Message);
-		UE_LOG(LogTemp, Warning, TEXT("Recieved Message %s"), *Message);
-	});
+		{
+			GEngine->AddOnScreenDebugMessage(-1, -15.0f, FColor::Cyan, "Recieved Message" + Message);
+			UE_LOG(LogTemp, Warning, TEXT("Recieved Message %s"), *Message);
+		});
 
 	//Event that triggers when client sends message to server
 	WebSocket->OnMessageSent().AddLambda([](const FString& Message)
-	{
-		GEngine->AddOnScreenDebugMessage(-1, -15.0f, FColor::Cyan, "Recieved Message" + Message);
-		UE_LOG(LogTemp, Warning, TEXT("Sent message %s"), *Message);
-	});
-	
+		{
+			GEngine->AddOnScreenDebugMessage(-1, -15.0f, FColor::Cyan, "Recieved Message" + Message);
+			UE_LOG(LogTemp, Warning, TEXT("Sent message %s"), *Message);
+		});
+
 	WebSocket->Connect();
 }
 
